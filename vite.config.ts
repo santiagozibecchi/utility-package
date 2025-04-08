594bc7c36156709072ea563aa78f5e19da3d13cc
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2025-04-08 11:56:30 -0300

    chore: change proyect name

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, ('src/index.ts')),
-      name: 'utility-package'
+      name: 'utility-package-santiago'
     }
   },
   resolve: {
